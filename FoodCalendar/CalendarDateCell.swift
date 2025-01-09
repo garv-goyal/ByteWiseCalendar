53f93ce2f57b26a8b63a692cb245b925f15198d5
garv-goyal <gghg1811@gmail.com>
2025-01-10 01:24:14 +0400

    scrollview and fix dark mode on cell

--- FoodCalendar/CalendarDateCell.swift
+++ FoodCalendar/CalendarDateCell.swift
@@ -1,77 +1,3 @@
-//import SwiftUI
-//
-//struct CalendarDateCell: View {
-//    var date: Date
-//    @Binding var foodItems: [FoodItem]
-//    @Binding var isDarkMode: Bool // Added isDarkMode binding
-//    @State private var draggedItem: String?
-//
-//    var isNextMonthDate: Bool = false
-//
-//    var body: some View {
-//        ZStack(alignment: .topLeading) {
-//            if isSpecificDate(date) {
-//                RoundedRectangle(cornerRadius: 8)
-//                    .fill(Color.blue.opacity(0.3))
-//                    .overlay(
-//                        RoundedRectangle(cornerRadius: 8)
-//                            .stroke(Color.blue, lineWidth: 2)
-//                    )
-//            } else {
-//                (isNextMonthDate
-//                    ? (isDarkMode ? Color(UIColor.systemGray4) : Color.purple.opacity(0.1))
-//                    : (isDarkMode ? Color(UIColor.systemGray5) : Color.white))
-//                    .overlay(
-//                        RoundedRectangle(cornerRadius: 8)
-//                            .stroke(isDarkMode ? Color.white : Color.clear, lineWidth: 1)
-//                    )
-//            }
-//
-//            Text(dayString(from: date))
-//                .font(.headline)
-//                .foregroundColor(isDarkMode ? Color.white : Color.black)
-//                .padding(5)
-//
-//            VStack {
-//                Spacer()
-//                HStack(spacing: 0) {
-//                    ForEach(itemsForDate(date: date).prefix(3), id: \.self) { item in
-//                        Image(item.imageName)
-//                            .resizable()
-//                            .scaledToFit()
-//                            .frame(width: 33, height: 37)
-//                            .padding(4)
-//                            .onDrag {
-//                                draggedItem = item.imageName
-//                                return NSItemProvider(object: item.imageName as NSString)
-//                            }
-//                    }
-//                }
-//                Spacer()
-//            }
-//        }
-//        .cornerRadius(8) 
-//    }
-//
-//    func isSpecificDate(_ date: Date) -> Bool {
-//        let calendar = Calendar.current
-//        let specificDateComponents = DateComponents(year: 2024, month: 11, day: 2)
-//        let specificDate = calendar.date(from: specificDateComponents)!
-//        return calendar.isDate(date, inSameDayAs: specificDate)
-//    }
-//
-//    func dayString(from date: Date) -> String {
-//        let formatter = DateFormatter()
-//        formatter.dateFormat = "d"
-//        return formatter.string(from: date)
-//    }
-//
-//    func itemsForDate(date: Date) -> [FoodItem] {
-//        foodItems.filter { Calendar.current.isDate($0.date, inSameDayAs: date) }
-//    }
-//}
-
-
 import SwiftUI
 
 struct CalendarDateCell: View {
@@ -84,7 +10,6 @@ struct CalendarDateCell: View {
 
     var body: some View {
         ZStack(alignment: .topLeading) {
-            // Highlight today's date
             if isToday(date) {
                 RoundedRectangle(cornerRadius: 8)
                     .fill(Color.blue.opacity(0.3))
@@ -93,7 +18,6 @@ struct CalendarDateCell: View {
                             .stroke(Color.blue, lineWidth: 2)
                     )
             } else {
-                // Style other dates
                 (isNextMonthDate
                     ? (isDarkMode ? Color(UIColor.systemGray4) : Color.purple.opacity(0.1))
                     : (isDarkMode ? Color(UIColor.systemGray5) : Color.white))
@@ -103,48 +27,47 @@ struct CalendarDateCell: View {
                     )
             }
 
-            // Display day number
             Text(dayString(from: date))
                 .font(.headline)
                 .foregroundColor(isDarkMode ? Color.white : Color.black)
                 .padding(5)
 
-            // Display food items
             VStack {
                 Spacer()
-                HStack(spacing: 0) {
-                    ForEach(itemsForDate(date: date).prefix(3), id: \.self) { item in
-                        Image(item.imageName)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 33, height: 37)
-                            .padding(4)
-                            .onDrag {
-                                draggedItem = item.imageName
-                                return NSItemProvider(object: item.imageName as NSString)
-                            }
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 4) {
+                        ForEach(itemsForDate(date: date), id: \.id) { item in
+                            Image(item.imageName)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 35, height: 35)
+                                .padding(4)
+                                .onDrag {
+                                    draggedItem = item.imageName
+                                    return NSItemProvider(object: item.imageName as NSString)
+                                }
+                        }
                     }
                 }
+                .padding(.horizontal, 4)
                 Spacer()
             }
         }
         .cornerRadius(8)
     }
 
-    // Highlight today's date
     func isToday(_ date: Date) -> Bool {
         Calendar.current.isDateInToday(date)
     }
 
-    // Format the day number
     func dayString(from date: Date) -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = "d"
         return formatter.string(from: date)
     }
 
-    // Filter food items for the given date
     func itemsForDate(date: Date) -> [FoodItem] {
         foodItems.filter { Calendar.current.isDate($0.date, inSameDayAs: date) }
     }
 }
+
